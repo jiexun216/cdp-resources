eb2009132a20507174ea1a0facfc885e885692cd
jiexun216 <503995651@qq.com>
2021-04-14 11:25:49 +0800

    mdify

--- hook/operate.go
+++ hook/operate.go
@@ -25,12 +25,10 @@ import (
 
 //only the # cml-user//ds-parent-namespace//
 // cdp-resources admission is to delete resource limit
-func createDeleteResourceContextPatch(pod corev1.Pod, availableAnnotations map[string]string, annotations map[string]string, availableLabels map[string]string, labels map[string]string) ([]byte, error) {
+func createDeleteResourceContextPatch(pod corev1.Pod, availableAnnotations map[string]string, annotations map[string]string) ([]byte, error) {
 	var patch []patchOperation
 	// update Annotation to set admissionWebhookAnnotationStatusKey: "mutated"
 	patch = append(patch, updateAnnotation(availableAnnotations, annotations)...)
-	// add labels
-	patch = append(patch, updateLabels(availableLabels, labels)...)
 
 	// delete pod resourse
 	var size = len(pod.Spec.Containers)
@@ -69,21 +67,6 @@ func updateAnnotation(target map[string]string, added map[string]string) (patch 
 			})
 		}
 	}
-	return patch
-}
-
-func updateLabels(target map[string]string, added map[string]string) (patch []patchOperation) {
-	values := make(map[string]string)
-	for key, value := range added {
-		if target == nil || target[key] == "" {
-			values[key] = value
-		}
-	}
-	patch = append(patch, patchOperation{
-		Op:    "add",
-		Path:  "/metadata/labels",
-		Value: values,
-	})
 	return patch
 }
 
